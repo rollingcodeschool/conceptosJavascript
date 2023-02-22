37adcbd138504188e0fd258e29bd06512d09cf37
Emilse Arias <emilsearias@gmail.com>
2023-02-22 11:40:20 -0300

    agregamos el archivo variables

---
+++ js/1-variables.js
@@ -0,0 +1,32 @@
+//Esto es un comentario de una sola linea
+
+/*
+Esto es un comentario
+de 
+multiples lineas
+*/
+
+// mostrar comentarios o texto en el doc. html
+document.write('<h1>Hola mundo</h1>');
+
+// mostrar un mensaje en consola
+console.log("Prueba desde la consola");
+
+// mostrar un mensaje en una ventana emergente
+alert('Prueba de alert');
+
+// crear o declarar variables (var - let - const)
+let producto = 'ipad pro 2021';
+
+document.write(producto);
+
+// modificar el contenido de una variable
+producto = 'ipad pro 2023';
+
+document.write('<br>' + producto);
+
+let numero1 = 30;
+let numero2= 50
+
+document.write('<br>'+ (numero1+numero2) )
+
