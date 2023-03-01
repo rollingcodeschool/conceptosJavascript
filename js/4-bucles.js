52ff543503ca240e60da7e149284b6b6d5bff41b
Emilse Arias <emilsearias@gmail.com>
2023-03-01 10:58:09 -0300

    agregamos un ejemplo de do-while

--- js/4-bucles.js
+++ js/4-bucles.js
@@ -4,7 +4,7 @@
 // }
 
 
-let i = 20;
+let i = 2;
 
 while(i <= 10){
     document.write(`<p>Estes es el parrafo N:${i}</p>`);
@@ -12,7 +12,20 @@ while(i <= 10){
     // i++ es igual i= i+1
 }
 
-
 // do-while
+/*
+    do{
+        todas las lineas de codigo
+        cambiar la condicion logica
+    }while(condicion logica)
+*/ 
+
+let contador= 100;
+do{
+    document.write(`<p>Estes es el parrafo con do-while N:${contador}</p>`);
+    contador++;
+}while(contador <=10)
+
+
 
 // for
